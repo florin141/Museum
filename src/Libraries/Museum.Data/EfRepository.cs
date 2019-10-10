72fc84b1d5037b8a7180a272f16f228377f8f007
florin141 <29834996+florin141@users.noreply.github.com>
2019-10-10 18:40:43 +0200

    Implement generic repository.

--- src/Libraries/Museum.Data/EfRepository.cs
+++ src/Libraries/Museum.Data/EfRepository.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,46 +12,99 @@ namespace Museum.Data
 {
     public class EfRepository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
     {
+        private readonly DbContext _context;
+
+        private DbSet<TEntity> _entities;
+
+        public EfRepository(DbContext context)
+        {
+            _context = context;
+        }
+
         public TEntity GetById(object id)
         {
-            throw new NotImplementedException();
+            return Entities.Find(id);
         }
 
         public IEnumerable<TEntity> GetAll()
         {
-            throw new NotImplementedException();
+            return Entities.ToList();
         }
 
         public void Insert(TEntity entity)
         {
-            throw new NotImplementedException();
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            Entities.Add(entity);
+            _context.SaveChanges();
         }
 
         public void Insert(IEnumerable<TEntity> entities)
         {
-            throw new NotImplementedException();
+            if (entities == null)
+                throw new ArgumentNullException(nameof(entities));
+
+            Entities.AddRange(entities);
+            _context.SaveChanges();
         }
 
         public void Update(TEntity entity)
         {
-            throw new NotImplementedException();
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            Entities.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.SaveChanges();
         }
 
         public void Update(IEnumerable<TEntity> entities)
         {
-            throw new NotImplementedException();
+            if (entities == null)
+                throw new ArgumentNullException(nameof(entities));
+
+            foreach (var entity in entities)
+            {
+                Entities.Attach(entity);
+                _context.Entry(entity).State = EntityState.Modified;
+            }
+
+            _context.SaveChanges();
         }
 
         public void Delete(TEntity entity)
         {
-            throw new NotImplementedException();
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            Entities.Remove(entity);
+            _context.SaveChanges();
         }
 
         public void Delete(IEnumerable<TEntity> entities)
         {
-            throw new NotImplementedException();
+            if (entities == null)
+                throw new ArgumentNullException(nameof(entities));
+
+            Entities.RemoveRange(entities);
+            _context.SaveChanges();
         }
 
-        public IQueryable<TEntity> Table { get; }
+        public IQueryable<TEntity> Table
+        {
+            get { return Entities; }
+        }
+
+        protected virtual DbSet<TEntity> Entities
+        {
+            get
+            {
+                if (_entities == null)
+                    _entities = _context.Set<TEntity>();
+
+                return _entities;
+            }
+        }
     }
 }
