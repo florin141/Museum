4cd8b1ef88e3006990bf0f7f217461eb00d81a87
florin141 <29834996+florin141@users.noreply.github.com>
2019-10-10 23:16:44 +0200

    Implement AreaService.

---
+++ src/Libraries/Museum.Service/Areas/AreaService.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Museum.Core.Data;
+using Museum.Core.Domain;
+
+namespace Museum.Service.Areas
+{
+    public class AreaService : IAreaService
+    {
+        private readonly IRepository<Area> _areaRepository;
+
+        public AreaService(IRepository<Area> areaRepository)
+        {
+            _areaRepository = areaRepository;
+        }
+
+        public Area GetAreaById(int areaId)
+        {
+            if (areaId == 0)
+                return null;
+
+            return _areaRepository.GetById(areaId);
+        }
+
+        public IEnumerable<Area> GetAllAreas()
+        {
+            return _areaRepository.GetAll();
+        }
+
+        public void InsertArea(Area area)
+        {
+            if (area == null)
+                throw new ArgumentNullException(nameof(area));
+
+            _areaRepository.Insert(area);
+        }
+
+        public void InsertAreas(IEnumerable<Area> areas)
+        {
+            if (areas == null)
+                throw new ArgumentNullException(nameof(areas));
+
+            _areaRepository.Insert(areas);
+        }
+
+        public void UpdateArea(Area area)
+        {
+            if (area == null)
+                throw new ArgumentNullException(nameof(area));
+
+            _areaRepository.Update(area);
+        }
+
+        public void UpdateAreas(IEnumerable<Area> areas)
+        {
+            if (areas == null)
+                throw new ArgumentNullException(nameof(areas));
+
+            _areaRepository.Update(areas);
+        }
+
+        public void DeleteArea(Area area)
+        {
+            if (area == null)
+                throw new ArgumentNullException(nameof(area));
+
+            _areaRepository.Delete(area);
+        }
+
+        public void DeleteAreas(IEnumerable<Area> areas)
+        {
+            if (areas == null)
+                throw new ArgumentNullException(nameof(areas));
+
+            _areaRepository.Delete(areas);
+        }
+    }
+}
---
+++ src/Libraries/Museum.Service/Museum/IMuseumService.cs
@@ -0,0 +1,34 @@
+﻿using Museum.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Museum.Service.Museum
+{
+    public interface IMuseumService
+    {
+        IEnumerable<Museums> GetAllMuseums(
+            DateTime? createdFromUtc = null, 
+            DateTime? createdToUtc = null,
+            int affiliateId = 0, 
+            int vendorId = 0, 
+            int[] customerRoleIds = null,
+            string email = null, 
+            string username = null, 
+            string firstName = null, 
+            string lastName = null,
+            int dayOfBirth = 0, 
+            int monthOfBirth = 0,
+            string company = null, 
+            string phone = null, 
+            string zipPostalCode = null, 
+            string ipAddress = null,
+            int pageIndex = 0, 
+            int pageSize = int.MaxValue, 
+            bool getOnlyTotalCount = false);
+
+
+    }
+}
